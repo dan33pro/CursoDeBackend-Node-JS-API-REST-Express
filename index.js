68377ac07235b6f5a023f44c68d73c0aecdc5eaa
daniel cespedes <daniel@DanielC-PC>
2022-12-14 13:44:35 -0500

    i express and server HTTP

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Mi tienda');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Este es mi server en Express');
+});
+app.listen(port, () => {
+    console.log('Corrinedo en: http://localhost:' + port);
+});
