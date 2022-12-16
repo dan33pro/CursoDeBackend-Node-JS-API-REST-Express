7a552f1946cc8574ab06c75efd0694b52a9c836e
daniel cespedes <daniel@DanielC-PC>
2022-12-16 13:27:46 -0500

    POST users

--- routes/products.router.js
+++ routes/products.router.js
@@ -38,6 +38,7 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   const body = req.body;
+  products.push(body);
   res.json({
     message: 'created',
     data: body,
--- routes/users.router.js
+++ routes/users.router.js
@@ -30,4 +30,13 @@ router.get('/', (req, res) => {
   }
 });
 
+router.post('/', (req, res) => {
+  const body = req.body;
+  users.push(body);
+  res.json({
+    message: 'created',
+    data: body,
+  });
+});
+
 module.exports = router;
