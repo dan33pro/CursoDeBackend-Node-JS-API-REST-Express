01c670e41106ae1be919c895a139ed124fde24aa
daniel cespedes <daniel@DanielC-PC>
2022-12-17 08:18:06 -0500

    GET: user by id

--- routes/users.router.js
+++ routes/users.router.js
@@ -30,6 +30,12 @@ router.get('/', (req, res) => {
   }
 });
 
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const user = users.filter((user) => user.id == id);
+  res.json(user);
+});
+
 router.post('/', (req, res) => {
   const body = req.body;
   users.push(body);
