66a00d79876f3376c7297f47caf7312aacac20a7
DagoGres <dan333pro@gmai.com>
2023-06-06 18:30:47 -0500

    Cambios heroku, no terminados

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+const whitelist = ['http://localhost:8080', 'https://myapp.com.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido en este origen'));
+    }
+  }
+};
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('<h1>Este es mi server en Express</h1>');
